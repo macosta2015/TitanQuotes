6b488d8b14fe3c76598b65c9d23f44dfb0ad3af6
Mario Acosta <macosta2015@my.fit.edu>
2023-09-29 15:58:00 -0400

    Starting up files

---
+++ src/App.js
@@ -0,0 +1,19 @@
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+        Hello World
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
